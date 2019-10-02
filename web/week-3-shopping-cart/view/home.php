7ab59fb8e39722e5dc54998b45d253d239342903
Jose Beltran <jbeltran@doterra.com>
2019-10-02 16:11:11 -0600

    cart controller

--- web/week-3-shopping-cart/view/home.php
+++ web/week-3-shopping-cart/view/home.php
@@ -46,42 +46,15 @@
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <form action="cart/index.php" method="POST">
-                    <textarea class="card-text" name="product-name" value="Product 1"
-                        readonly=readonly>Product 1</textarea>
+                    <p class="card-text" readonly=readonly>Product 1 p</p>
+                    <input type="hidden" name="product-name" name="product-name" value="Product 1 hidden">
                     <p class="card-text" name="product-price" value="55.00">price</p>
-
-                    <input type="submit" class="btn btn-primary" name="submit" value="BUY NOW" style="visibility :hidden;" id="primaryButton">
+                    <input type="submit" class="btn btn-primary" name="submit" value="BUY NOW" id="primaryButton">
                     <input type="hidden" name="action" value="addToCart">
                 </form>
             </div>
         </div>
 
-
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary" onclick="document.getElementById('primaryButton').click()">VIEW CART</button>
-      </div>
-    </div>
-  </div>
-</div>
 
     </main>
 
