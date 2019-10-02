55d6337ea2df140e6574e87579c0b851b997f0c5
Jose Beltran <jbeltran@doterra.com>
2019-10-02 12:13:37 -0600

    test script

--- web/week-3-shopping-cart/view/home.php
+++ web/week-3-shopping-cart/view/home.php
@@ -31,8 +31,12 @@
             <img src="..." class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
-                <p class="card-text">card text.</p>
-                <a href="#" class="btn btn-primary">BUY NOW</a>
+                <form action="cart.php" method="post">
+                    <p class="card-text" name="product-name">name</p>
+                    <p class="card-text" name="product-price">price</p>
+                    <p class="card-text" name="product-description">Description</p>
+                    <button type="submit" class="btn btn-primary">BUY NOW</button>
+                </form>
             </div>
         </div>
     </main>
