e040d8bc6a9268ab6786d1ecc83a5aa9b64f0586
Jose Beltran <jbeltran@doterra.com>
2019-10-02 15:04:27 -0600

    cart controller

--- web/week-3-shopping-cart/view/home.php
+++ web/week-3-shopping-cart/view/home.php
@@ -50,11 +50,34 @@
                         readonly=readonly>Product 1</textarea>
                     <p class="card-text" name="product-price" value="55.00">price</p>
 
-                    <input type="submit" class="btn btn-primary" name="submit" value="BUY NOW">
+                    <input type="submit" class="btn btn-primary" name="submit" value="BUY NOW" data-toggle="modal" data-target="#exampleModal">
                     <input type="hidden" name="action" value="addToCart">
                 </form>
             </div>
         </div>
+
+
+        <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
     </main>
 
     <!-- FOOTER -->
