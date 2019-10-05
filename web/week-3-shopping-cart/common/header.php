74e34efe9cf1ca1b9e2c68f1f75ddfc4a28826d0
Jose Beltran <jbeltran@doterra.com>
2019-10-05 17:47:43 -0600

    cart btn

--- web/week-3-shopping-cart/common/header.php
+++ web/week-3-shopping-cart/common/header.php
@@ -1,6 +1,9 @@
 <header>
-    <a class="btn btn-success" href="/week-3-shopping-cart/view/cart.php">CART</a>
-    <?php 
-        echo count($_SESSION['cart']);
-    ?>
+    <a class="btn btn-success" href="/week-3-shopping-cart/view/cart.php">
+        CART 
+        <?php 
+            echo " " + count($_SESSION['cart']);
+        ?>
+    </a>
+
 </header>
